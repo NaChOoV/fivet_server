c955d1f0553384a83bf11505bd13a1300b4c97ca
git@github.com:NaChOoV <fuenzalida.veas@gmail.com>
2020-05-18 19:35:39 -0400

    feat(Program): implement server

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 
-namespace fivet_server
+namespace Fivet.ZeroIce
 {
-    class Program
-    {
-        static void Main(string[] args)
+
+    
+    class TheSystemImpl : model.TheSystemDisp_ {
+         public override long getDelay(long clientTime, Ice.Current current)
         {
-            Console.WriteLine("Hello World!");
+            return DateTime.Now.Ticks - clientTime;
         }
     }
+
+    class Program
+    {
+        
+        static int Main(string[] args)
+        {
+            try
+            {
+                using(Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                {
+                    var adapter =
+                        communicator.createObjectAdapterWithEndpoints("TheAdapter", "default -p 8080 -z");
+                    adapter.add(new TheSystemImpl(), Ice.Util.stringToIdentity("cl.ucn.disc.pdis.fivet.zeroice.model.TheSystem"));
+                    adapter.activate();
+
+                    Console.WriteLine("Waiting for connections ..");
+
+                    communicator.waitForShutdown();
+
+                    
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+
+
+
+    
 }
